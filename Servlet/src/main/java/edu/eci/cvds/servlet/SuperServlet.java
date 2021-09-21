fac783c898b6b717049fe2aeac2840040247a4b6
Sebastian Ospina <juansebastian200103@gmail.com>
2021-09-21 16:51:50 -0500

    se crea la clase SuperServlet

---
+++ Servlet/src/main/java/edu/eci/cvds/servlet/SuperServlet.java
@@ -0,0 +1,22 @@
+package edu.eci.cvds.servlet;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Optional;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+    urlPatterns = "/lab5"
+)
+public class SuperServlet extends HttpServlet{
+    static final long serialVersionUID = 35L;
+
+    @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       
+   }
+}
